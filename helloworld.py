1360d9eee25ce0e3ed82d6321504bddc7548177b
spoorthi-bojja <sbojja@cbu.edu>
2025-08-09 06:23:09 +0300

    Adding operators

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+# Introduction
+# Day one of 30-Days-Of Python
+print(2+2)  #Addition Operator(+)
+print(2-2)  #Subtraction Operator(-)
+print(2/2)  #Division Operator(/)
+print(2*2)  #Multiplication Operator(*)
+print(2**2) #Exponential Operator(**)
+print(2% 2) #Modulo Operator(%)
+print(3//2) #Floor Division Operator(//)
