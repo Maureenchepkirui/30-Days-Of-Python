4ef4673cb3f3a6fec9c1e3f80d2fe17d3c7d472d
spoorthi-bojja <sbojja@cbu.edu>
2025-08-09 06:44:52 +0300

    Checkin Data Types

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,8 @@
 # Introduction
 # Day one of 30-Days-Of Python
+
+#Operators
+
 print(2+2)  #Addition Operator(+)
 print(2-2)  #Subtraction Operator(-)
 print(2/2)  #Division Operator(/)
@@ -7,3 +10,16 @@ print(2*2)  #Multiplication Operator(*)
 print(2**2) #Exponential Operator(**)
 print(2% 2) #Modulo Operator(%)
 print(3//2) #Floor Division Operator(//)
+
+
+# Checking Data Types
+print(type(20)) #Integer
+print(type(2.7)) #Float
+print(type('Maureen')) #String
+print(type(3+4j)) # complex
+print(type([1,2,3,4,5])) #List
+print(type({'name':'Maureen','Age':27,'Occupation':'Data Engineer'})) #Dictionary
+print (type({9.2,9.4,7.8})) #set
+print(type((9.2,9.4,7.8))) #Tuple
+print(type(3==7)) #Boolean
+print(type(3>=8)) #Boolean
